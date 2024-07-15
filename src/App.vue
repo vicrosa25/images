84c6cc6c679bfd2e288471012c53d0c0aefdee8f
Victor Rosa <vicrosa25@gmail.com>
2024-07-15 09:30:52 +0200

    First commint

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <AppHeader></AppHeader>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader
+  }
+}
+</script>
